c8c21b60a3b129fd982c8f24598cc3e37e5fb4b1
Filip <szymanski.fp@gmail.com>
2022-06-05 16:27:27 +0200

    allowed cors from frontend

--- backend/src/main/java/eu/filip/backend/config/CorsConfig.java
+++ backend/src/main/java/eu/filip/backend/config/CorsConfig.java
@@ -1,4 +1,28 @@
 package eu.filip.backend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
